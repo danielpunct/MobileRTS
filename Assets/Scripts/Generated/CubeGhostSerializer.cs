d000e5b377da9eaba0d0fc8d081bb11d12cb7310
Daniel Lupascu <liviudaniel.lupascu@gmail.com>
2020-02-07 20:14:16 +0100

    Basic multiplayer cube logic on localhost

---
+++ Assets/Scripts/Generated/CubeGhostSerializer.cs
@@ -0,0 +1,68 @@
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Entities;
+using Unity.Collections;
+using Unity.NetCode;
+using Unity.Physics;
+using Unity.Rendering;
+using Unity.Transforms;
+
+public struct CubeGhostSerializer : IGhostSerializer<CubeSnapshotData>
+{
+    private ComponentType componentTypePhysicsCollider;
+    private ComponentType componentTypePerInstanceCullingTag;
+    private ComponentType componentTypeLocalToWorld;
+    private ComponentType componentTypeRotation;
+    private ComponentType componentTypeTranslation;
+    // FIXME: These disable safety since all serializers have an instance of the same type - causing aliasing. Should be fixed in a cleaner way
+    [NativeDisableContainerSafetyRestriction][ReadOnly] private ArchetypeChunkComponentType<Rotation> ghostRotationType;
+    [NativeDisableContainerSafetyRestriction][ReadOnly] private ArchetypeChunkComponentType<Translation> ghostTranslationType;
+
+
+    public int CalculateImportance(ArchetypeChunk chunk)
+    {
+        return 1;
+    }
+
+    public bool WantsPredictionDelta => true;
+
+    public int SnapshotSize => UnsafeUtility.SizeOf<CubeSnapshotData>();
+    public void BeginSerialize(ComponentSystemBase system)
+    {
+        componentTypePhysicsCollider = ComponentType.ReadWrite<PhysicsCollider>();
+        componentTypePerInstanceCullingTag = ComponentType.ReadWrite<PerInstanceCullingTag>();
+        componentTypeLocalToWorld = ComponentType.ReadWrite<LocalToWorld>();
+        componentTypeRotation = ComponentType.ReadWrite<Rotation>();
+        componentTypeTranslation = ComponentType.ReadWrite<Translation>();
+        ghostRotationType = system.GetArchetypeChunkComponentType<Rotation>(true);
+        ghostTranslationType = system.GetArchetypeChunkComponentType<Translation>(true);
+    }
+
+    public bool CanSerialize(EntityArchetype arch)
+    {
+        var components = arch.GetComponentTypes();
+        int matches = 0;
+        for (int i = 0; i < components.Length; ++i)
+        {
+            if (components[i] == componentTypePhysicsCollider)
+                ++matches;
+            if (components[i] == componentTypePerInstanceCullingTag)
+                ++matches;
+            if (components[i] == componentTypeLocalToWorld)
+                ++matches;
+            if (components[i] == componentTypeRotation)
+                ++matches;
+            if (components[i] == componentTypeTranslation)
+                ++matches;
+        }
+        return (matches == 5);
+    }
+
+    public void CopyToSnapshot(ArchetypeChunk chunk, int ent, uint tick, ref CubeSnapshotData snapshot, GhostSerializerState serializerState)
+    {
+        snapshot.tick = tick;
+        var chunkDataRotation = chunk.GetNativeArray(ghostRotationType);
+        var chunkDataTranslation = chunk.GetNativeArray(ghostTranslationType);
+        snapshot.SetRotationValue(chunkDataRotation[ent].Value, serializerState);
+        snapshot.SetTranslationValue(chunkDataTranslation[ent].Value, serializerState);
+    }
+}

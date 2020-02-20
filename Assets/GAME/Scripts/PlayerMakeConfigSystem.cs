d44f1f0437fa2347d39fda87bb0ab74461e4eb5d
Daniel Lupascu <liviudaniel.lupascu@gmail.com>
2020-02-20 22:54:09 +0100

    Cubes instantiated from config

---
+++ Assets/GAME/Scripts/PlayerMakeConfigSystem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.NetCode;
+using UnityEngine;
+
+[UpdateInGroup(typeof(ServerSimulationSystemGroup))]
+public class PlayerMakeConfigSystem : ComponentSystem
+{
+
+    protected override void OnUpdate()
+    {
+        Entities.WithAll<PlayerConfig>().ForEach((Entity entity, ref PlayerConfig config) =>
+        {
+            var ghostCollection = GetSingleton<GhostPrefabCollectionComponent>();
+            var ghostId = MobileRTSGhostSerializerCollection.FindGhostType<CubeSnapshotData>();
+            var prefab = EntityManager.GetBuffer<GhostPrefabBuffer>(ghostCollection.serverPrefabs)[ghostId].Value;
+
+            for (int i = 0; i < config.Units; i++)
+            {
+                var unit = EntityManager.Instantiate(prefab);
+            }
+
+            PostUpdateCommands.RemoveComponent<PlayerConfig>(entity);
+        });
+    }
+}

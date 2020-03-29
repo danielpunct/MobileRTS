fcc9ba0651ab2c8cde1d729a90557202588b025e
Daniel Lupascu <liviudaniel.lupascu@gmail.com>
2020-03-29 18:18:37 +0200

    Units die

--- Assets/GAME/Scripts/UnitHitSystem.cs
+++ Assets/GAME/Scripts/UnitHitSystem.cs
@@ -1,18 +1,57 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.NetCode;
+using Unity.Transforms;
 
-public class UnitHitSystem : MonoBehaviour
+[UpdateInGroup(typeof(ServerSimulationSystemGroup))]
+[UpdateAfter(typeof(UnitsMoveSystem))]
+public class UnitHitSystem : JobComponentSystem
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
-        
-    }
+        var ECB = new EntityCommandBuffer(Allocator.Temp);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Entities.WithAll<PlayerBullet>().WithoutBurst().ForEach((
+            Entity entity,
+            in Translation translation) =>
+        {
+            var query = GetEntityQuery(ComponentType.ReadOnly<Health>(), ComponentType.ReadOnly<Translation>());
+            var positions = query.ToComponentDataArray<Translation>(Allocator.TempJob);
+            var unitsHealth = query.ToComponentDataArray<Health>((Allocator.TempJob));
+            var entityes_native = query.ToEntityArray(Allocator.TempJob);
+
+            for (int i = 0; i < positions.Length; i++)
+            {
+                var distance = math.distance(positions[i].Value, translation.Value);
+                if (distance < 2)
+                {
+                    // move the removal in another system?
+                    var new_health =  unitsHealth[i].Value- 10;
+                    UnityEngine.Debug.Log("take");
+                    if (new_health > 0)
+                    {
+                        ECB.SetComponent(entityes_native[i], new Health { Value = new_health });
+                    }
+                    else
+                    {
+                        ECB.DestroyEntity(entityes_native[i]);
+                    }
+
+                    ECB.DestroyEntity(entity);
+                }
+            }
+
+            positions.Dispose();
+            unitsHealth.Dispose();
+            entityes_native.Dispose();
+
+        }).Run();
+
+        ECB.Playback(EntityManager);
+        ECB.Dispose();
+
+        return default;
     }
 }
